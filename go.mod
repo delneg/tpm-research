1ce46f55eaf65d608d58f0ff1f46eabc76165963
Denis <dev-adv@swisstronik.com>
2024-08-09 12:55:36 +0400

    tpm ecdh

--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module tpm-research
 
 go 1.22
 
-require github.com/google/go-tpm v0.9.1
+toolchain go1.22.2
 
-require golang.org/x/sys v0.8.0 // indirect
+require (
+	github.com/google/go-cmp v0.5.9
+	github.com/google/go-tpm v0.9.1
+)
+
+require (
+	github.com/google/go-tpm-tools v0.3.13-0.20230620182252-4639ecce2aba // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,40 +1,161 @@
 package main
 
 import (
+	"crypto/ecdh"
+	"crypto/rand"
+	"encoding/hex"
 	"flag"
-	"fmt"
-	"os"
-
-	"github.com/google/go-tpm/legacy/tpm2"
-	"github.com/google/go-tpm/tpmutil"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	. "github.com/google/go-tpm/tpm2"
+	"github.com/google/go-tpm/tpm2/transport"
+	"github.com/google/go-tpm/tpm2/transport/simulator"
+	"log"
 )
 
 var (
 	tpmPath = flag.String("tpm-path", "/dev/tpm0", "Path to the TPM device (character device or a Unix socket)")
 	index   = flag.Uint("index", 0, "NVRAM index of read")
+	useSim  = flag.Bool("simulator", false, "Use simulator instead of TPM")
 )
 
+func doStuff(ttpm transport.TPM) {
+
+	// Create a TPM ECDH key
+	tpmCreate := CreatePrimary{
+		PrimaryHandle: TPMRHOwner,
+		InPublic: New2B(TPMTPublic{
+			Type:    TPMAlgECC,
+			NameAlg: TPMAlgSHA256,
+			ObjectAttributes: TPMAObject{
+				FixedTPM:             true,
+				STClear:              false,
+				FixedParent:          true,
+				SensitiveDataOrigin:  true,
+				UserWithAuth:         true,
+				AdminWithPolicy:      false,
+				NoDA:                 true,
+				EncryptedDuplication: false,
+				Restricted:           false,
+				Decrypt:              true,
+				SignEncrypt:          false,
+				X509Sign:             false,
+			},
+			Parameters: NewTPMUPublicParms(
+				TPMAlgECC,
+				&TPMSECCParms{
+					CurveID: TPMECCNistP256,
+					Scheme: TPMTECCScheme{
+						Scheme: TPMAlgECDH,
+						Details: NewTPMUAsymScheme(
+							TPMAlgECDH,
+							&TPMSKeySchemeECDH{
+								HashAlg: TPMAlgSHA256,
+							},
+						),
+					},
+				},
+			),
+		}),
+	}
+	log.Printf("Created tpmCreate %+v", tpmCreate)
+
+	// Use NIST P-256
+	curve := ecdh.P256()
+
+	tpmCreateRsp, err := tpmCreate.Execute(ttpm)
+	if err != nil {
+		log.Fatalf("could not create the TPM key: %+v", err)
+	}
+	log.Printf("TpmCreateResponse: %+v", tpmCreateRsp)
+	outPub, err := tpmCreateRsp.OutPublic.Contents()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	log.Printf("TPM public key: %+v", outPub)
+	tpmPub, err := outPub.Unique.ECC()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	log.Printf("TPM public key out: %+v", tpmPub)
+	tpmPubKey, err := ECDHPubKey(curve, tpmPub)
+	if err != nil {
+		log.Fatalf("could not unmarshall pubkey: %+v", err)
+	}
+	log.Printf("TPM public key ecdh: %+v", tpmPubKey)
+
+	// Create a SW ECDH key
+	swPriv, err := curve.GenerateKey(rand.Reader)
+	if err != nil {
+		log.Fatalf("could not create the SW key: %+v", err)
+	}
+	log.Printf("SW key: %+v", swPriv)
+	x, y, err := ECCPoint(swPriv.PublicKey())
+	log.Printf("ECC x: %+v, y: %+v", x, y)
+	if err != nil {
+		log.Fatalf("could not get SW key point: %+v", err)
+	}
+	swPub := TPMSECCPoint{
+		X: TPM2BECCParameter{Buffer: x.FillBytes(make([]byte, 32))},
+		Y: TPM2BECCParameter{Buffer: y.FillBytes(make([]byte, 32))},
+	}
+
+	// Calculate Z based on the SW priv * TPM pub
+	zx, err := swPriv.ECDH(tpmPubKey)
+	if err != nil {
+		log.Fatalf("ecdh exchange: %+v", err)
+	}
+	log.Printf("ECC zx: %s", hex.EncodeToString(zx))
+
+	z := TPMSECCPoint{
+		X: TPM2BECCParameter{Buffer: zx},
+	}
+
+	// Calculate Z based on TPM priv * SW pub
+	ecdh2 := ECDHZGen{
+		KeyHandle: AuthHandle{
+			Handle: tpmCreateRsp.ObjectHandle,
+			Name:   tpmCreateRsp.Name,
+			Auth:   PasswordAuth(nil),
+		},
+		InPoint: New2B(swPub),
+	}
+	log.Printf("ECdh2 %+v", ecdh2)
+	ecdhRsp, err := ecdh2.Execute(ttpm)
+	if err != nil {
+		log.Fatalf("ECDH_ZGen failed: %+v", err)
+	}
+	log.Printf("ECDH2 execute response: %+v", ecdhRsp)
+
+	outPoint, err := ecdhRsp.OutPoint.Contents()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	log.Println()
+	log.Printf("z.X: %s, outpoint.X: %s", hex.EncodeToString(z.X.Buffer), hex.EncodeToString(outPoint.X.Buffer))
+	if !cmp.Equal(z.X, outPoint.X, cmpopts.IgnoreUnexported(z.X)) {
+		log.Printf("want %x got %x", z, outPoint)
+	}
+}
 func main() {
+
 	flag.Parse()
 
-	if *index == 0 {
-		fmt.Fprintln(os.Stderr, "--index must be set")
-		os.Exit(1)
+	if *useSim {
+		ttpm, err := simulator.OpenSimulator()
+		if err != nil {
+			log.Fatalf("could not connect to TPM simulator: %+v", err)
+		}
+		defer ttpm.Close()
+		doStuff(ttpm)
+	} else {
+		ttpm, err := transport.OpenTPM("/dev/tpm0")
+		if err != nil {
+			log.Fatalf("could not connect to TPM: %+v", err)
+		}
+		defer ttpm.Close()
+
+		doStuff(ttpm)
 	}
 
-	val, err := nvRead(*tpmPath, uint32(*index))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "reading from index 0x%x: %v\n", *index, err)
-		os.Exit(1)
-	}
-	fmt.Printf("NVRAM value at index 0x%x (hex encoded):\n%x\n", *index, val)
-}
-
-func nvRead(path string, index uint32) ([]byte, error) {
-	rwc, err := tpm2.OpenTPM(path)
-	if err != nil {
-		return nil, fmt.Errorf("can't open TPM at %q: %v", path, err)
-	}
-	defer rwc.Close()
-	return tpm2.NVRead(rwc, tpmutil.Handle(index))
 }
